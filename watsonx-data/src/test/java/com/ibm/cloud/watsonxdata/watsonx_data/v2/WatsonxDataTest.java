a4a4f178a536c91f12f1eb3376644df98643da5e
Yash Gogoriya <Yash.Gogoriya1@ibm.com>
2025-09-18 13:29:28 +0530

    feat(sdk): update SDK to include 2.2.0 SaaS changes
    Signed-off-by: Yash Gogoriya <Yash.Gogoriya1@ibm.com>

--- watsonx-data/src/test/java/com/ibm/cloud/watsonxdata/watsonx_data/v2/WatsonxDataTest.java
+++ watsonx-data/src/test/java/com/ibm/cloud/watsonxdata/watsonx_data/v2/WatsonxDataTest.java
@@ -2,7 +2,7 @@
  * (C) Copyright IBM Corp. 2025.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * the License. You may obtain a copy of the License at 
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
