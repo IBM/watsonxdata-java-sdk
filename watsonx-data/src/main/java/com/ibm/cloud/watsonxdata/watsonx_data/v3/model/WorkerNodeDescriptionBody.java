22679712886dc4e0bbaac14c90d19c7c64ac9501
Maritta Stephen <Maritta.Stephen@ibm.com>
2026-01-26 16:27:29 +0530

    feat(v2 SDK) : Added v3 SDK with new v3 API definition

--- watsonx-data/src/main/java/com/ibm/cloud/watsonxdata/watsonx_data/v3/model/WorkerNodeDescriptionBody.java
+++ watsonx-data/src/main/java/com/ibm/cloud/watsonxdata/watsonx_data/v3/model/WorkerNodeDescriptionBody.java
@@ -14,7 +14,6 @@
 package com.ibm.cloud.watsonxdata.watsonx_data.v3.model;
 import com.google.gson.annotations.SerializedName;
 import com.ibm.cloud.sdk.core.service.model.GenericModel;
-
 /**
  * Presto engine configurations.
  */
