0f594db2948dae392d5976ef5cfcc199bc85bd9b
Travis CI User <travis@example.org>
2026-01-30 11:35:13 +0000

    doc: latest javadoc for 0.9.0 (05085e3b0492f73698cec3bb52614f004c6708b2)

---
+++ docs/0.9.0/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.ibm.cloud.watsonx_data.common"},{"l":"com.ibm.cloud.watsonxdata.watsonx_data.v3"},{"l":"com.ibm.cloud.watsonxdata.watsonx_data.v3.model"}]
